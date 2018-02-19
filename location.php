3b512613ef8825ecdf85841b8b82108f6395f2da
Keval Morabia <kevalmorabia97@gmail.com>
2018-02-20 00:41:58 +0530

    Update location.php

--- location.php
+++ location.php
@@ -1,4 +1,7 @@
 <?php
+	/*
+		In this file, update lat and lng of the tracked device using GPS module
+	*/
 	$lat = 23.6838716;
 	$lng = 73.37194090000003;
 	echo '{"lat":"'.$lat.'", "lng":"'.$lng.'"}';
